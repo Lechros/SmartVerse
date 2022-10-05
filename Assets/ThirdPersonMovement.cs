8029823bf3c2ae99a4c14a8ae613df612332ec04
OniOniOn <0onionion0@gmail.com>
2022-10-05 19:07:25 +0900

    feat: add multiplayer first and third camera

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,54 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonMovement : NetworkBehaviour
+{
+    public CharacterController controller;
+    public Transform frontFacing;
+
+    public float speed = 6;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3;
+
+    public float groundDistance = 0.4f;
+
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+
+    private void Start()
+    {
+        frontFacing = Camera.main.transform;
+        if(isLocalPlayer)
+        {
+            var tpc_cmFl = GameObject.Find("Third Person Camera").GetComponent<CinemachineFreeLook>();
+            tpc_cmFl.Follow = this.transform;
+            tpc_cmFl.LookAt = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+        //walk
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + frontFacing.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
