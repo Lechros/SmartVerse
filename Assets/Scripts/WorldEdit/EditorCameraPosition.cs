580b85d105194fab9f5c73440aeafdcdfca23dc2
OniOniOn <0onionion0@gmail.com>
2022-12-07 06:25:10 +0900

    feat: add shift fast move in editor

--- Assets/Scripts/WorldEdit/EditorCameraPosition.cs
+++ Assets/Scripts/WorldEdit/EditorCameraPosition.cs
@@ -4,6 +4,7 @@ public class EditorCameraPosition : MonoBehaviour
 {
     private Transform frontFacing;
     public float moveSpeed;
+    public float fastMoveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +15,10 @@ public class EditorCameraPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float multiplier = Input.GetKey(KeyCode.LeftShift) ? fastMoveSpeed : moveSpeed;
         Vector3 move = frontFacing.forward * Input.GetAxis("Vertical")
              + frontFacing.right * Input.GetAxis("Horizontal")
              + frontFacing.up * Input.GetAxis("Keyboard Y Axis");
-        transform.position += Vector3.ClampMagnitude(move, 1) * moveSpeed * Time.deltaTime;
+        transform.position += Vector3.ClampMagnitude(move, 1) * multiplier * Time.deltaTime;
     }
 }
